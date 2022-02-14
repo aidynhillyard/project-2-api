0e6e5905ea493356dcc5a4c09b89452e85b95ec9
Aidyn Hillyard <aidyn@Aidyns-MacBook-Pro.local>
2022-02-14 12:31:42 -0600

    updated css, changed component information

--- all-gender-restrooms/src/Components/RestroomDetailCard.js
+++ all-gender-restrooms/src/Components/RestroomDetailCard.js
@@ -2,7 +2,7 @@ import {useParams} from 'react-router';
 import {useState, useEffect} from 'react';
 
 const RestroomDetailCard = ({ toggle, handleAddRestroomList }) => {
-    const [singleRestroom, setSingleRestroom] = useState({})
+    const [singleRestroom, setSingleRestroom] = useState([])
 
     let params = useParams()
 
@@ -11,23 +11,32 @@ const RestroomDetailCard = ({ toggle, handleAddRestroomList }) => {
     }, [toggle])
 
     const getSingleRestroom = () => {
-        const url = "https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=30&offset=0&ada=true&query=burnsville"
+        const url = "https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=30&offset=0&query=burnsville" 
+            // (!params.name ? singleRestroom[0]?.name : params.name)
 
         fetch(url)
             .then((response) => response.json())
             .then((data) => setSingleRestroom(data))
             .catch(() => console.log("There are no results."))
     }
-    // console.log(singleRestroom[0].name)
-    return (
+
+    const restroomList = singleRestroom.map((soloRestroom, id) => (
         <div className="restroom-detail-card">
-            {/* <h2><span className="center-align">{singleRestroom[0].name}</span></h2>
-            <h3>{singleRestroom[0].street}</h3>
-            <h3>{singleRestroom[0].city}</h3>
-            <h3>{singleRestroom[0].state}</h3> */}
+            <h2><span className="center-align">{(soloRestroom?.name) ? (soloRestroom?.name) : ''}</span></h2>
+            <h3>{soloRestroom?.street ? soloRestroom?.street : ''}</h3>
+            <h3>{soloRestroom?.city ? soloRestroom?.city : ''}</h3>
+            <h3>{soloRestroom?.state ? soloRestroom?.state : ''}</h3>
             <h3>Images for: Unisex, ADA, Changing Table</h3>
-            <button onClick={() => handleAddRestroomList(singleRestroom[0].name)}>Add to List</button>
+            <button onClick={() => handleAddRestroomList(soloRestroom?.name)}>Add to List</button>
         </div>
+    ))
+
+    console.log(restroomList)
+
+    return (
+        <>
+            {restroomList}
+        </>
     )
 }
 
--- all-gender-restrooms/src/Components/RestroomSearch.js
+++ all-gender-restrooms/src/Components/RestroomSearch.js
@@ -5,40 +5,83 @@ import RestroomDetailCard from './RestroomDetailCard';
 const RestroomSearch = ({ handleAddRestroomList }) => {
 
     // This will set useStates
-    const [restroomList, setRestroomList] = useState([])
+    // const [restroomList, setRestroomList] = useState([])
     const [toggle, setToggle] = useState(true)
+    const [keyword, setKeyword] = useState('')
+    const [restrooms, setRestrooms] = useState([])
 
-    // This will run getRestroomList on page load; it gets the list of All-Gender Restrooms.
-    useEffect (() => {
-        getRestroomList()
-    }, [])
-
-    const getRestroomList = () => {
-        const url = "https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=30&offset=0&ada=true&query=burnsville"
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        const url = `https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=30&offset=0&query=${keyword}`;
+    
         fetch(url)
             .then((response) => response.json())
-            .then((data) => setRestroomList(data))
+            .then((data) => setRestrooms(data))
             .catch(() => console.log("There are no search results."))
-        }
+    }
 
-    const listOfRestrooms = restroomList.map((restroom, id) => (
+    const handleChange = (event) => {
+        setKeyword(event.target.value);
+      };
+
+    const restroomList = restrooms.map((restroom, id) => (
         <li className="restroom-list" key={id}>
             {/* On-click toggle will toggle the RestroomDetailCard Component */}
             <Link onClick={() => setToggle(!toggle)} to={`/restroomsearch/${restroom.name}`} key={id}>{restroom.name}</Link>
         </li>
-        )
-    )
+    ))
 
-    console.log(restroomList)
+    //   console.log(restrooms.name)
+    //   console.log(restroomList)
 
-    return (
-        <div className="restroom-search">
-            <ul className="full-restroom-list">{listOfRestrooms}</ul>
-            <RestroomDetailCard toggle={toggle}
-            handleAddRestroomList={handleAddRestroomList}/>
+      return (
+        <div className="search-container">
+          <form className="search-form" onSubmit={handleSubmit}>
+            <label>Keyword: </label>
+            <input type="text" value={keyword} onChange={handleChange} />
+            <input type="submit" value="Find Restroom Info" />
+          </form>
+          <div className="restroom-search">
+            {restroomList}
+            <h2>Restroom Name: {restrooms[0]?.name}</h2>
+            <h3>Restroom City: {restrooms[0]?.city}</h3>
+            <RestroomDetailCard toggle={toggle} handleAddRestroomList={handleAddRestroomList}/>
+          </div>
         </div>
-    )
+      );
+
+    // // This will run getRestroomList on page load; it gets the list of All-Gender Restrooms.
+    // useEffect (() => {
+    //     getRestroomList()
+    // }, [])
+
+    // const getRestroomList = () => {
+    //     const url = "https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=30&offset=0&ada=true&query=burnsville"
+
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data) => setRestroomList(data))
+    //         .catch(() => console.log("There are no search results."))
+    //     }
+
+    // const listOfRestrooms = restroomList.map((restroom, id) => (
+    //     <li className="restroom-list" key={id}>
+    //         {/* On-click toggle will toggle the RestroomDetailCard Component */}
+    //         <Link onClick={() => setToggle(!toggle)} to={`/restroomsearch/${restroom.name}`} key={id}>{restroom.name}</Link>
+    //     </li>
+    //     )
+    // )
+
+    // console.log(restroomList)
+
+    // return (
+    //     <div className="restroom-search">
+    //         <ul className="full-restroom-list">{listOfRestrooms}</ul>
+    //         <RestroomDetailCard toggle={toggle}
+    //         handleAddRestroomList={handleAddRestroomList}/>
+    //     </div>
+    // )
 }
 
 export default RestroomSearch;
