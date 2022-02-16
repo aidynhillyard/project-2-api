116ff7bc2e10ac352577056addb61bddaebcf908
Aidyn Hillyard <aidyn@Aidyns-MacBook-Pro.local>
2022-02-16 17:43:14 -0600

    modified additional file paths for home page

--- src/App.js
+++ src/App.js
@@ -38,9 +38,9 @@ function App() {
           {/* The :name is a parameter we can now store */}
           <Route path="/restroomsearch/:name" element={<RestroomSearch handleAddRestroomList={handleAddRestroomList}/>} />
           <Route path="/mylist" element={<MyList restroomList={restroomList} handleRemoveRestroomList={handleRemoveRestroomList}/>} />
-          <Route path="/" element = {<Navigate to="/restroomsearch" />} />
-          <Route path="/restroom-search" element = {<Navigate to="/restroom-search" />} />
-          <Route path="/search" element = {<Navigate to="/restroom-search" />} />
+          <Route path="/" element = {<Navigate to="/restroomresource" />} />
+          <Route path="/restroom-resource" element = {<Navigate to="/restroomresource" />} />
+          <Route path="/search" element = {<Navigate to="/restroomresource" />} />
         </Routes>
       </main>
       {/* Footer has name and social media icons */}
